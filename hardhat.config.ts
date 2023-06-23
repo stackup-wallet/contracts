6977709040cfdad09c6e6ecb1a8c89a255e2ec47
NickSolante <nicksolante1@gmail.com>
2023-06-23 11:29:37 +1000

    feat: base support

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -47,6 +47,7 @@ const config: HardhatUserConfig = {
       bscTestnet: process.env.BSCSCAN_API_KEY || "",
       arbitrumOne: process.env.ARBISCAN_API_KEY || "",
       arbitrumGoerli: process.env.ARBISCAN_API_KEY || "",
+      baseGoerli: process.env.BASESCAN_API_KEY || "",
     },
   },
   gasReporter: {
@@ -74,6 +75,7 @@ const config: HardhatUserConfig = {
     bscTestnet: getChainConfig(process.env.BSC_TESTNET_RPC),
     arbitrumOne: getChainConfig(process.env.ARBITRUM_ONE_RPC),
     arbitrumGoerli: getChainConfig(process.env.ARBITRUM_GOERLI_RPC),
+    baseGoerli: getChainConfig(process.env.BASE_GOERLI_RPC),
   },
   paths: {
     artifacts: "./artifacts",
