62d84f1d2ba97b5329788b03560877c57996349d
NickSolante <nicksolante1@gmail.com>
2023-07-03 16:57:04 +1000

    feat: base-contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -49,6 +49,16 @@ const config: HardhatUserConfig = {
       arbitrumGoerli: process.env.ARBISCAN_API_KEY || "",
       baseGoerli: process.env.BASESCAN_API_KEY || "",
     },
+    customChains: [
+      {
+        network: "baseGoerli",
+        chainId: 84531,
+        urls: {
+          apiURL: "https://api-goerli.basescan.org/api",
+          browserURL: "https://goerli.basescan.org",
+        },
+      },
+    ],
   },
   gasReporter: {
     currency: "USD",
