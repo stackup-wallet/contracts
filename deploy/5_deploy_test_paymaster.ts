03f33d9b24fac14ddcbcb5d2fb6376c68b54f5d3
hazim <hazim.jumali@gmail.com>
2023-11-29 16:54:59 +1100

    Add TestPaymaster for bundler E2E testing (#142)

---
+++ deploy/5_deploy_test_paymaster.ts
@@ -0,0 +1,23 @@
+import { ethers } from "hardhat";
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const provider = ethers.provider;
+  const from = await provider.getSigner().getAddress();
+
+  await hre.deployments.deploy("TestPaymaster", {
+    from,
+    log: true,
+    deterministicDeployment: true,
+  });
+};
+
+func.skip = async (_: HardhatRuntimeEnvironment): Promise<boolean> => {
+  const runOnly = process.env.RUN_ONLY?.split(",");
+  if (!runOnly || runOnly.length === 0) return false;
+
+  return !runOnly.includes("5");
+};
+
+export default func;
